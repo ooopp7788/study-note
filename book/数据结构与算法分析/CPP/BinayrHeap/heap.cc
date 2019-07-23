41ea41bf20170a2484fe2f44ddec3cd8f3c6b42e
liumengjie04 <liumengjie04@meituan.com>
2019-07-23 17:27:35 +0800

    二叉堆

---
+++ book/数据结构与算法分析/CPP/BinayrHeap/heap.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "heap.h"
+
+template <typename Comparable>
+void insert(const Comparable & x) {
+  if (currentSize == array.size() - 1)
+    array.resize(array.size() * 2);
+  int hole = ++currentSize();
+  for ( ; hole > 1 && x < array[hole / 2]; hole /= 2)
+    array[hole] = array[hole / 2];
+  array[hole] = x;
+}
+
+template <typename Comparable>
+void BinaryHeap<Comparable>::deleteMin() {
+  int hole = 1;
+  array[hole] = array[currentSize--];
+  percolateDown(hole);
+}
+
+template <typename Comparable>
+void BinaryHeap<Comparable>::percolateDown(int hole) {
+  int child;
+  Comparable tmp = array[hole];
+
+  for ( ; hole * 2 <= currentSize; hole = child ) {
+    child = hole * 2;
+    if (child != currentSize && array[child + 1] < array[child])
+      child ++;
+    if (array[child] < array[hole])
+      array[hole] = array[child];
+    else
+      break;
+  }
+  array[hole] = temp;
+}
---
+++ book/数据结构与算法分析/CPP/BinayrHeap/heap.h
@@ -0,0 +1,21 @@
+template <typename Comparable>
+class BinaryHeap {
+  public:
+    explicit BinaryHeap(int capacity = 100);
+    explicit BinaryHeap(const vector<Comparable> & items);
+
+    bool isEmpty() const;
+    const Comparable & findMin() const;
+
+    void insert(const Comparable & x);
+    void deleteMin();
+    void deleteMin(Comparable & minItem);
+    void makeEmpty();
+
+  private:
+    int currentSize;
+    vector<Comparable> array;
+
+    void buildHeap();
+    void percolateDown(int hole);
+};
