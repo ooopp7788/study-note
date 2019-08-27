5b96490d81bf0dabcc048e0458e822785acfd265
liumengjie04 <liumengjie04@meituan.com>
2019-08-27 19:59:24 +0800

    链表删除Nth节点

--- leetcode/js/removeNthFromEnd.js
+++ leetcode/js/removeNthFromEnd.js
@@ -11,5 +11,21 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  
+  var ptr = head;
+  var ptr2 = head;
+  var prev = null;
+  while(n) {
+    ptr = ptr.next;
+    n --;
+  }
+  while(ptr) {
+    ptr = ptr.next;
+    prev = ptr2;
+    ptr2 = ptr2.next;
+  }
+  if (!prev) {
+    return head.next;
+  }
+  prev.next = ptr2.next;
+  return head;
 };
