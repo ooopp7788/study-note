58c85ea6bfab20b430c6fddabd37fb4346d3f144
liumengjie04 <liumengjie04@meituan.com>
2020-01-03 22:18:44 +0800

    changCoin

--- leetcode/exchangeCoins.js
+++ leetcode/exchangeCoins.js
@@ -1,8 +1,9 @@
-function exchange(coins, n) {
+function coinChange(coins, n) {
   function _findCoin(n) {
     let l = coins.length
     while(l --) {
       const coin = coins[l]
+      console.log(coin, n)
       if (n >= coin) {
         coins = coins.slice(0, l + 1)
         return coin
@@ -12,10 +13,22 @@ function exchange(coins, n) {
   }
   const coin = _findCoin(n)
   if (coin && n > 0) {
-    return [coin, ...exchange(coins, n - coin)]
+    const count = Math.floor(n / coin)
+    return count + coinChange(coins, n - coin * count)
   }
-  return []
+  return -1
+}
+console.log(coinChange([1,2,5],11))
+function sums(coins, sum) {
+  if (sum === 0) {
+    return 1
+  } else if (sum < 0) {
+    return 0
+  }
+  if (coins.length === 0) {
+    return 0
+  }
+  return sums(coins, sum - coins[0]) + sums(coins.slice(1), sum)
 }
 
-coins = [1, 5, 10, 50];
-console.log(exchange(coins, 6))
+// console.log(sums([1,2,3,4,5,6], 7))
