5246ad0cef7ad2693c3762cbe6c7813a71e0138c
liumengjie04 <liumengjie04@meituan.com>
2020-01-09 14:28:30 +0800

    dp

--- leetcode/DP/exchangeCoins.js
+++ leetcode/DP/exchangeCoins.js
@@ -1,34 +1,25 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
 function coinChange(coins, n) {
-  function _findCoin(n) {
-    let l = coins.length
-    while(l --) {
-      const coin = coins[l]
-      console.log(coin, n)
-      if (n >= coin) {
-        coins = coins.slice(0, l + 1)
-        return coin
+  let arr = new Array(n + 1).fill(-1)
+  arr[0] = 0
+  let i = 1
+  while (i <= n) {
+    for (coin of coins) {
+      if (i - coin >= 0 && arr[i - coin] >= 0) {
+        if (arr[i] < 0) {
+          arr[i] = arr[i - coin] + 1
+        } else {
+          arr[i] = Math.min(1 + arr[i - coin], arr[i])
+        }
       }
     }
-    return false
+    i++
   }
-  const coin = _findCoin(n)
-  if (coin && n > 0) {
-    const count = Math.floor(n / coin)
-    return count + coinChange(coins, n - coin * count)
-  }
-  return -1
-}
-console.log(coinChange([1,2,5],11))
-function sums(coins, sum) {
-  if (sum === 0) {
-    return 1
-  } else if (sum < 0) {
-    return 0
-  }
-  if (coins.length === 0) {
-    return 0
-  }
-  return sums(coins, sum - coins[0]) + sums(coins.slice(1), sum)
+  return arr[n]
 }
 
-// console.log(sums([1,2,3,4,5,6], 7))
+console.log(coinChange([2],3))
